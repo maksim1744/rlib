ff8453c4b5aab9668263132fc551223a441c1651
Maksim Gorokhovskii <magor1744@gmail.com>
2022-10-04 15:43:58 +0300

    allow unused cin/cout

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -5,7 +5,9 @@ pub mod writer;
 #[macro_export]
 macro_rules! make_io {
     ($reader:ident, $writer:ident) => {
+        #[allow(unused_variables)]
         let mut $reader = rlib_io::reader::Reader::new(Box::new(std::io::stdin().lock()));
+        #[allow(unused_variables)]
         let mut $writer = rlib_io::writer::Writer::new(Box::new(std::io::stdout().lock()));
 
         rlib_io::output_macro::make_output_macro!($writer);
