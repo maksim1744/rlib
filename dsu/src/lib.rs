b3cb7d0ac695a8edd589767bfd100895d3b4d773
Maksim Gorokhovskii <magor1744@gmail.com>
2022-10-06 17:34:33 +0300

    add dsu

---
+++ dsu/src/lib.rs
@@ -0,0 +1,51 @@
+#[derive(Clone, Debug)]
+pub struct DSU {
+    p: Vec<usize>,
+    sz: Vec<usize>,
+}
+
+impl DSU {
+    pub fn new(n: usize) -> Self {
+        Self {
+            p: (0..n).collect(),
+            sz: vec![1; n],
+        }
+    }
+
+    pub fn reset(&mut self, n: usize) {
+        self.p.resize(n, 0);
+        for i in 0..n {
+            self.p[i] = i;
+        }
+        self.sz.resize(n, 0);
+        for i in 0..n {
+            self.sz[i] = 1;
+        }
+    }
+
+    pub fn par(&mut self, v: usize) -> usize {
+        if self.p[v] != v {
+            self.p[v] = self.par(self.p[v]);
+        }
+        self.p[v]
+    }
+
+    pub fn un(&mut self, mut u: usize, mut v: usize) -> bool {
+        u = self.par(u);
+        v = self.par(v);
+        if u == v {
+            return false;
+        }
+        if self.sz[u] > self.sz[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.sz[v] += self.sz[u];
+        self.p[u] = v;
+        true
+    }
+
+    pub fn size(&mut self, v: usize) -> usize {
+        let v = self.par(v);
+        self.sz[v]
+    }
+}
---
+++ dsu/tests/tests.rs
@@ -0,0 +1,43 @@
+use rlib_dsu::*;
+
+#[test]
+fn simple() {
+    let mut dsu = DSU::new(4);
+    assert!(dsu.un(0, 1));
+    let root = dsu.par(0);
+    assert!(root == 0 || root == 1);
+
+    assert!(dsu.un(1, 2));
+    assert_eq!(dsu.par(0), root);
+    assert_eq!(dsu.par(1), root);
+    assert_eq!(dsu.par(2), root);
+    assert_eq!(dsu.par(3), 3);
+    assert_eq!(dsu.size(0), 3);
+    assert_eq!(dsu.size(1), 3);
+    assert_eq!(dsu.size(2), 3);
+
+    assert!(dsu.un(3, 0));
+    assert_eq!(dsu.par(0), root);
+    assert_eq!(dsu.par(1), root);
+    assert_eq!(dsu.par(2), root);
+    assert_eq!(dsu.par(3), root);
+    assert_eq!(dsu.size(0), 4);
+    assert_eq!(dsu.size(1), 4);
+    assert_eq!(dsu.size(2), 4);
+    assert_eq!(dsu.size(3), 4);
+
+    dsu.reset(3);
+    for i in 0..3 {
+        assert_eq!(dsu.par(i), i);
+        assert_eq!(dsu.size(i), 1);
+    }
+
+    dsu.un(0, 1);
+    dsu.un(1, 2);
+
+    dsu.reset(10);
+    for i in 0..10 {
+        assert_eq!(dsu.par(i), i);
+        assert_eq!(dsu.size(i), 1);
+    }
+}
