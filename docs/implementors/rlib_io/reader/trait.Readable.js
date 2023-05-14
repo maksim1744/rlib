b9e5d94a5cfae5eb5d3d7665ad7eb7197b27df33
CI <you@example.com>
2023-05-14 11:12:28 +0000

    Update docs

--- docs/implementors/rlib_io/reader/trait.Readable.js
+++ docs/implementors/rlib_io/reader/trait.Readable.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "rlib_io":[],
-"rlib_mint":[["impl&lt;const M: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.69.0/std/primitive.i32.html\">i32</a>&gt; <a class=\"trait\" href=\"rlib_io/reader/trait.Readable.html\" title=\"trait rlib_io::reader::Readable\">Readable</a> for <a class=\"struct\" href=\"rlib_mint/struct.Modular.html\" title=\"struct rlib_mint::Modular\">Modular</a>&lt;M&gt;"]]
+"rlib_mint":[["impl&lt;const M: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.69.0/std/primitive.u32.html\">u32</a>&gt; <a class=\"trait\" href=\"rlib_io/reader/trait.Readable.html\" title=\"trait rlib_io::reader::Readable\">Readable</a> for <a class=\"struct\" href=\"rlib_mint/struct.Modular.html\" title=\"struct rlib_mint::Modular\">Modular</a>&lt;M&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
