a0a1feef7e197f6c603927f0c44aef949e42c91e
CI <you@example.com>
2024-05-30 11:29:29 +0000

    Update docs

--- docs/trait.impl/rlib_num_traits/trait.ZeroOne.js
+++ docs/trait.impl/rlib_num_traits/trait.ZeroOne.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"rlib_f80":[["impl <a class=\"trait\" href=\"rlib_num_traits/trait.ZeroOne.html\" title=\"trait rlib_num_traits::ZeroOne\">ZeroOne</a> for <a class=\"struct\" href=\"rlib_f80/struct.f80.html\" title=\"struct rlib_f80::f80\">f80</a>"]],
+"rlib_f80":[["impl ZeroOne for <a class=\"struct\" href=\"rlib_f80/struct.f80.html\" title=\"struct rlib_f80::f80\">f80</a>"]],
 "rlib_fft":[["impl&lt;F: Float&gt; ZeroOne for <a class=\"struct\" href=\"rlib_fft/struct.Complex.html\" title=\"struct rlib_fft::Complex\">Complex</a>&lt;F&gt;"]],
 "rlib_num_traits":[],
 "rlib_rational":[["impl&lt;T: ZeroOne&gt; ZeroOne for <a class=\"struct\" href=\"rlib_rational/struct.Rational.html\" title=\"struct rlib_rational::Rational\">Rational</a>&lt;T&gt;"]]
