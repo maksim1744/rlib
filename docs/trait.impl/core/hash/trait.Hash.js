75963f4dedbeef29132708d7f6a0ca952f00fa9f
CI <ci@github.com>
2025-11-03 11:20:56 +0000

    Update docs

--- docs/trait.impl/core/hash/trait.Hash.js
+++ docs/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rlib_rational",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rlib_rational/struct.Rational.html\" title=\"struct rlib_rational::Rational\">Rational</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["rlib_rational",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rlib_rational/struct.Rational.html\" title=\"struct rlib_rational::Rational\">Rational</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
